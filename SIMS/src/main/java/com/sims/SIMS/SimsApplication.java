eb5ae5535c398a54c818eb552ef91f234d4438bb
jeujeon <1132jjw@naver.com>
2022-11-17 11:16:04 +0900

    feat: 프로젝트의 구조 생성

---
+++ SIMS/src/main/java/com/sims/SIMS/SimsApplication.java
@@ -0,0 +1,13 @@
+package com.sims.SIMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimsApplication.class, args);
+	}
+
+}
---
+++ SIMS/src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Spring Boot Application</title>
+</head>
+<body>Hello, Spring Boot App</body>
+</html>
